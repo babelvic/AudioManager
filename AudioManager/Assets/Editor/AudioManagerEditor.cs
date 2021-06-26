86304c86deb8bcc8afef565f01f13ec150feaa54
Víctor <victorb.belchi18720@gmail.com>
2021-06-27 00:16:50 +0200

    Refactoring code

--- AudioManager/Assets/Editor/AudioManagerEditor.cs
+++ AudioManager/Assets/Editor/AudioManagerEditor.cs
@@ -153,6 +153,7 @@ public class AudioManagerEditor : UnityEditor.Editor
          }
      }
 
+     //Utility Function
      void FindMixerByID(int id, int index)
      {
          if (manager.mixers.Count >= 1)
@@ -162,7 +163,6 @@ public class AudioManagerEditor : UnityEditor.Editor
                  manager.tracks[index].mixer = manager.mixers[id].mixerGroup;
              }
          }
-         Debug.Log(manager.mixers[id].mixerGroup);
      }
 
      //Property Drawer for the tracks class
